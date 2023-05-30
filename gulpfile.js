045156b3912c9b506606a7e5df1ae3adc7800b30
TKuijvenhoven <thomaskuijv@gmail.com>
2023-05-30 13:38:22 +0200

    added dist/
    added dist/

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
 const gulp = require('gulp');
-const concat = require('gulp-concat');
 const sass = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
 
 gulp.task('styles', function() {
-    return gulp.src('sass/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(concat('palletnet.css'))
-        .pipe(gulp.dest('dist/css'));
+  return gulp.src('src/sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(concat('palletnet.css'))
+    .pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('scripts', function() {
-    return gulp.src('js/*.js')
-        .pipe(concat('palletnet.js'))
-        .pipe(gulp.dest('dist/js'));
+  return gulp.src('src/js/*.js')
+    .pipe(concat('palletnet.js'))
+    .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('default', gulp.parallel('styles', 'scripts'));
